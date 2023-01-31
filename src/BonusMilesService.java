c4218b99eaa68649824e02ce650bad8d780635a3
ksenia <ksy184694@gmail.com>
2023-01-31 19:49:07 +0300

    modernization task

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+        int amountForOneBonus = 20;
+        int bonusMiles;
+        return bonusMiles = cost / amountForOneBonus;
+
+    }
+}
